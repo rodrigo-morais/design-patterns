4d0c844a87af117a61b29a54784e9e8de47eadfb
Rodrigo Morais <morais.rm@gmail.com>
2015-12-08 22:22:41 -0200

    Creating the Subscriber and Observer classes.

---
+++ examples/design-patterns/observer/Observer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_patterns.observer
+{
+    public class Observer
+    {
+        private string name = "";
+        private decimal value = 0;
+
+        public Observer(string name, decimal value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+
+        public void notify()
+        {
+            Console.WriteLine(this.name + " - " + this.value);
+        }
+    }
+}
---
+++ examples/design-patterns/observer/Subscriber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_patterns.observer
+{
+    public class Subscriber
+    {
+        List<Observer> observers = new List<Observer>();
+
+
+        public void subscribe(Observer observer)
+        {
+            this.observers.Add(observer);
+        }
+
+        public void unsubscribe(Observer observer)
+        {
+            this.observers.Remove(observer);
+        }
+
+        public void update()
+        {
+            this.observers.ForEach(o => o.notify());
+        }
+
+    }
+}

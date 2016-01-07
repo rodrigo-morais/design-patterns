97911cdf71bbad280d08c9489315817c9ae6f171
Rodrigo Morais <morais.rm@gmail.com>
2016-01-06 23:14:14 -0200

    Creating a model to iterator example.

--- examples/design-patterns/Program.cs
+++ examples/design-patterns/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
+using design_patterns.iterator;
 using design_patterns.observer;
 using design_patterns.proxy;
 
@@ -56,6 +56,18 @@ namespace design_patterns
 
             Console.WriteLine("Sum 3 + 8: {0}", mathProxy.sum(3, 8));
             Console.WriteLine("Subtraction 13 + 8: {0}", mathProxy.subtraction(13, 8));
+        }
+
+        public static void iterator()
+        {
+            var task = new Task();
+
+
+            while (task.hasNext())
+            {
+                task = task.next();
+                Console.WriteLine(task.Value());
+            }
         }
     }
 }
---
+++ examples/design-patterns/iterator/Task.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_patterns.iterator
+{
+    public class Task
+    {
+        public string Value { get; set; }
+
+        public Task next()
+        {
+            return null;
+        }
+
+        public Boolean hasNext()
+        {
+            return false;
+        }
+    }
+}
